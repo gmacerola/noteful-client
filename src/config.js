b9922eedaac26daefb29d1328068708d1ec51e93
Gianpaolo Macerola <gmacerola@gmail.com>
2020-11-07 09:52:35 -0600

    fixed add folder and note

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
   // API_ENDPOINT: 'http://localhost:9090'
-  API_ENDPOINT: "https://nameless-gorge-83479.herokuapp.com/",
+  API_ENDPOINT: "https://nameless-gorge-83479.herokuapp.com/api",
 };
--- src/notes-helpers.js
+++ src/notes-helpers.js
@@ -1,15 +1,13 @@
+export const findFolder = (folders = [], folderId) =>
+  folders.find((folder) => folder.id === Number(folderId));
 
-export const findFolder = (folders=[], folderId) =>
-  folders.find(folder => folder.id === folderId)
+export const findNote = (notes = [], noteId) =>
+  notes.find((note) => note.id === Number(noteId));
 
-export const findNote = (notes=[], noteId) =>
-  notes.find(note => note.id === noteId)
-
-export const getNotesForFolder = (notes=[], folderId) => (
-  (!folderId)
+export const getNotesForFolder = (notes = [], folderId) =>
+  !folderId
     ? notes
-    : notes.filter(note => note.folderId === folderId)
-)
+    : notes.filter((note) => note.folderId === Number(folderId));
 
-export const countNotesForFolder = (notes=[], folderId) =>
-  notes.filter(note => note.folderId === folderId).length
+export const countNotesForFolder = (notes = [], folderId) =>
+  notes.filter((note) => note.folderId === Number(folderId)).length;
